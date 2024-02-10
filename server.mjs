c1de9e855472d85fe2f47f80eaebda758d264e83
ayomide <mikaheelayomide5@gmail.com>
2024-02-10 18:10:02 +0100

    server side js done

---
+++ server.mjs
@@ -0,0 +1,47 @@
+import stripe from 'stripe';
+import express from 'express';
+const app = express();
+
+let theStripeGateway = stripe("sk_test_51OScz1FDiGCp6miEnWUQfp7d0Jmd5phX3s2aQqi3Q1Ufbf0PHRWY7CbGemcgn5FUMpgCEpmoRTmWiJv2FQMIQrnx00vEP6MgQW");
+
+// app.use(express.json()); 
+
+app.post('/stripe-checkout', async (req,res) => {
+  console.log("Request body:", req.body);
+try {
+  const session = await theStripeGateway.checkout.sessions.create({
+    payment_method_types: ["card"],
+    mode: "payment",
+    success_url: "http://localhost:3000/success.html",
+    cancel_url: "http://localhost:3000/cancel.html",
+    line_items: req.body.everyThing.map(item => {
+      return {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: item.name,
+            description: items.desc,
+            images: [item.img]
+          },
+          unit_amount: item.price * 100
+        },
+        quantity: item.item
+      }
+    })
+  });
+  console.log("Session object:", session); 
+  res.json(session.url);
+} catch (error) {
+  console.log("error:", error);
+  res.status(500).json({ error: "An errror occured during checkout."});
+}
+});
+
+
+
+
+
+// Start server
+//app.listen(3000, () => {
+ // console.log('Server is running on port 3000');
+//});
