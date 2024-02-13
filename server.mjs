350f42576b76548468bd9c978d054a765d889f65
ayomide <mikaheelayomide5@gmail.com>
2024-02-13 15:26:54 +0100

    sorting the error

--- server.mjs
+++ server.mjs
@@ -4,7 +4,7 @@ const app = express();
 
 let theStripeGateway = stripe("sk_test_51OScz1FDiGCp6miEnWUQfp7d0Jmd5phX3s2aQqi3Q1Ufbf0PHRWY7CbGemcgn5FUMpgCEpmoRTmWiJv2FQMIQrnx00vEP6MgQW");
 
-// app.use(express.json()); 
+app.use(express.json()); 
 
 app.post('/stripe-checkout', async (req,res) => {
   console.log("Request body:", req.body);
@@ -12,16 +12,16 @@ try {
   const session = await theStripeGateway.checkout.sessions.create({
     payment_method_types: ["card"],
     mode: "payment",
-    success_url: "http://localhost:3000/success.html",
-    cancel_url: "http://localhost:3000/cancel.html",
-    line_items: req.body.everyThing.map(item => {
+    success_url: "http://localhost:8158/success.html",
+    cancel_url: "http://localhost:8158/cancel.html",
+    line_items: req.body.items.map(item => {
       return {
         price_data: {
           currency: "usd",
           product_data: {
             name: item.name,
             description: items.desc,
-            images: [item.img]
+  //          images: [item.img]
           },
           unit_amount: item.price * 100
         },
@@ -41,7 +41,6 @@ try {
 
 
 
-// Start server
-//app.listen(3000, () => {
- // console.log('Server is running on port 3000');
-//});
+app.listen(3000, () => {
+ console.log('Server is running on port 3000');
+});
